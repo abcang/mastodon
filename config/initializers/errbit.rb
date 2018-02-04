5859aa3eb349a2a36fcd0faa7850e422c4442a14
ABCanG <abcang1015@gmail.com>
2018-02-04 18:04:10 +0900

    Improve ignore exceptions

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -8,10 +8,24 @@ Airbrake.configure do |config|
   config.ignore_environments = %w(development test)
 end
 
+ERRBIT_IGNORE_ERRORS = %w[
+  Mastodon::UnexpectedResponseError
+  OpenSSL::SSL::SSLError
+  Excon::Error::ServiceUnavailable
+  Excon::Error::Timeout
+  ActiveRecord::ConnectionTimeoutError
+  SignalException
+].freeze
+
+ERRBIT_IGNORE_WORKER_ERRORS = {
+  'ActiveRecord::RecordInvalid' => ['ActivityPub::ProcessingWorker']
+}.freeze
+
 Airbrake.add_filter do |notice|
   if notice[:errors].any? { |error|
     error[:type].constantize.ancestors.include?(HTTP::Error) ||
-    ['Mastodon::UnexpectedResponseError', 'OpenSSL::SSL::SSLError', 'Excon::Error::ServiceUnavailable', 'Excon::Error::Timeout', 'ActiveRecord::ConnectionTimeoutError'].include?(error[:type])
+    ERRBIT_IGNORE_ERRORS.include?(error[:type]) ||
+    ERRBIT_IGNORE_WORKER_ERRORS[error[:type]]&.inclide?(notice[:params].dig('job', 'class'))
   }
     notice.ignore!
   end
